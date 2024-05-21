1b21aac6fa8fbc8001777223822fa2adc6f83a27
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-21 10:52:04 -0400

    Create Payroll Record System
    Application is able to create employee records, as
    well as calculate time worked and wages earned.
    Finally we are able to process payroll for all employees
    and get the total amount owed.

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Your code here
+const createEmployeeRecord = (employee) => {
+  return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+}
+
+const createEmployeeRecords = (records) => {
+  const recordsArray = []
+  records.map(record => {
+    recordsArray.push(createEmployeeRecord(record))
+  })
+  return recordsArray
+}
+
+const createTimeInEvent = (employeeRecord, event) => {
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    date: event.slice(0,10),
+    hour: parseInt(event.slice(-4))
+  })
+  return employeeRecord
+}
+
+const createTimeOutEvent = (employeeRecord, event) => {
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    date: event.slice(0,10),
+    hour: parseInt(event.slice(-4))
+  })
+  return employeeRecord
+}
+
+const hoursWorkedOnDate = (employeeRecord, date) => {
+  const timeIn = employeeRecord.timeInEvents.find(event => event.date === date).hour
+  const timeOut = employeeRecord.timeOutEvents.find(event => event.date === date).hour
+  return (timeOut - timeIn ) / 100
+}
+
+const wagesEarnedOnDate = (employeeRecord, date) => {
+  const hoursWorked = hoursWorkedOnDate(employeeRecord, date)
+  return hoursWorked * employeeRecord.payPerHour
+}
+
+const allWagesFor = (employeeRecord) => {
+  const allDatesArray = employeeRecord.timeInEvents.map(event => event.date)
+  const allWagesArray = allDatesArray.map(date => wagesEarnedOnDate(employeeRecord,date))
+  return allWagesArray.reduce((acc,amount) => acc + amount)
+}
+
+const calculatePayroll = (employeeRecords) => {
+  const allWagesOwed = employeeRecords.map(record => allWagesFor(record))
+  return allWagesOwed.reduce((acc, amount) => acc + amount)
+}
